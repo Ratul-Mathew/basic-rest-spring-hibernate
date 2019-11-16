edb07e01a75af67f1cffdac043690f45c1926a84
Ratul <ratulphilip@gmail.com>
2019-11-16 10:44:36 +0530

    Basic structure for spring + mvn

---
+++ src/main/java/com/ratul/basicrestspringhibernate/BasicRestSpringHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.ratul.basicrestspringhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRestSpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicRestSpringHibernateApplication.class, args);
+	}
+
+}
